a00c8e2be297b110178bdad55269029a21ed1ce5
Nedifar <gazimoviljasilxamovich@gmail.com>
2022-12-15 14:06:02 +0500

    sks

--- src/pages/Group/Group.jsx
+++ src/pages/Group/Group.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Button from '@mui/material/Button'
 import "./groupstyle.css"
-import { IconButton, TextField } from "@mui/material";
+import { Alert, IconButton, TextField } from "@mui/material";
 import { DoorBackOutlined, SettingsAccessibility, Sledding } from "@mui/icons-material";
 import NewReleases from "@mui/icons-material/NewReleasesOutlined";
 import { Select } from "@mui/material";
@@ -13,19 +13,92 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
 import dayjs from 'dayjs'
 import 'dayjs/locale/ru'
 import axios from 'axios'
+import DateFormat from "./components/DateFormat";
+import ReloadCat from "./components/reloadcat.gif"
 
 function Group() {
     const [count, setCount] = useState(0);
-    const [value, setValue] = useState(dayjs('2022-04-07'));
+    const [value, setValue] = useState(dayjs(new Date().toDateString()));
     const [locale, setLocale] = useState('ru');
     const [spisok, setSpisok] = useState([]);
-
+    const [dateFormat, setDateFormat] = useState(new DateFormat(new Date()));
+    const [newSheduleIconVisible, setNewSheduleIconVisible] = useState(false);
     const handleGroupChange = (dayWeeks) => {
         setSpisok(getWeekBlocks(dayWeeks));
     }
+    const NewSheduleButton = () => {
+        if (newSheduleIconVisible) {
+            return (
+                <IconButton>
+                    <NewReleases style={iconStyle} />
+                </IconButton>);
+        }
+        else {
+            return null;
+        }
+    };
+
+    const CustomAlert = (text) => {
+        return (
+            <Alert className="errorAlert">{text}</Alert>
+        )
+    }
+
+    useEffect(() => {
+        let list = document.querySelectorAll('p[data-type="Day"]')
+        list.forEach(element => {
+            element.innerHTML = element.firstChild?.data.replace('\n', '<br>');
+            if (element.innerHTML == "undefined") {
+                element.innerHTML = "-";
+            }
+        });
+        let listBlocksForColors = document.querySelectorAll('.dayWeekContainer > div:first-child');
+        let listBlocksForColors1 = document.querySelectorAll('.dayWeekContent > div:first-child');
+        let colors = ["#B96CBD", "#49A24D", "#FDA838", "#F75355", "#00C6AE", "#455399"];
+        let counterColor = 0;
+        for (let i = 0; i < listBlocksForColors.length; i++) {
+            listBlocksForColors[i].style.backgroundColor = colors[counterColor];
+            listBlocksForColors1[i].style.backgroundColor = colors[counterColor];
+            if (counterColor < colors.length) {
+                counterColor++;
+            }
+            else {
+                counterColor = 0;
+            }
+        }
+    })
+
+    const handleDateChange = (newValue) => {
+        setDateFormat(new DateFormat(new Date(newValue.year(), newValue.month(), newValue.date())));
+        setValue(newValue);
+        let val = document.querySelector('.headerCenterBlock >div >input');
+        let img = document.querySelector('.reloadCat');
+        let itemsShedule = document.querySelectorAll('.shedule *:not(img)')
+        img.style = "opacity: 1; z-index: 3";
+        handleGroupChange([]);
+        let doc = document.querySelector('.css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input').value.split('.');
+        axios.get(`http://192.168.147.51:81/api/lastdance/getgroupmobile/${val.value}?Date=${doc[1]}.${doc[0]}.${doc[2]}`)
+            .then((response) => {
+                if (response.status === 200) {
+                    console.log(response.data);
+                    handleGroupChange(response.data);
+                }
+            }).catch(err => {
+
+                console.log(err);
+            })
+        setTimeout(() => {
+            img.style = "opacity: 0; z-index: 1";
+            itemsShedule = document.querySelectorAll('.shedule > *:not(img)');
+            itemsShedule.forEach(element => {
+                element.style = "";
+            });
+        }, 3000);
+    };
 
     const getWeekBlocks = (dayWeeks) => {
         let content = [];
+        let counter = 0;
         dayWeeks.forEach(element => {
             content.push(
                 <div className="buttonDayWeekContainer" key={element.dayWeekName}>
@@ -35,8 +108,8 @@ function Group() {
                         </div>
                         <div>
                             <div>
-                                <p>05</p>
-                                <span>декабрь</span>
+                                <p>{dateFormat.addDays(counter).getDate()}</p>
+                                <span>{dateFormat.getMonth(dateFormat.addDays(counter))}</span>
                             </div>
                             <div>
 
@@ -59,29 +132,31 @@ function Group() {
                                 </p>
                                 {getLessonBlock(element.dayWeekClasses)}
                             </div>
+
                         </div>
                     </div>
                 </div>
 
             );
+            counter++;
         });
         return content;
     };
 
     const getLessonBlock = (lessons) => {
         let lessonBlocks = [];
+        let counter = 0;
         lessons.forEach(element => {
             lessonBlocks.push(
-                <div>
+                <div key={element.Day + element.beginMobile}>
                     <p>{element.number}</p>
-                    <p>08:30-09:15</p>
-                    <p>
-                        {element.decipline}
-                    </p>
+                    <p data-type="Day" data-typedata={element.Day}>{element.day.replace('\n', "<br>")}</p>
+                    <p>{element.beginMobile}-{element.endMobile}</p>
                 </div>);
+
         });
         return lessonBlocks;
-        
+
     }
 
     const iconStyle = { fontSize: 45 }
@@ -90,12 +165,10 @@ function Group() {
         <div className="main">
             <div className="headerGrid">
                 <div className="leftIconsBlock">
-                    <IconButton>
+                    <IconButton >
                         <DoorBackOutlined style={iconStyle} />
                     </IconButton>
-                    <IconButton>
-                        <NewReleases style={iconStyle} />
-                    </IconButton>
+                    <NewSheduleButton />
                 </div>
                 <div className="headerCenterBlock">
                     <span>
@@ -104,7 +177,7 @@ function Group() {
                     <GroupSelect handleGroupChange={handleGroupChange} />
                 </div>
                 <div className="rightItemsBlock">
-                    <IconButton className="settingsButton">
+                    <IconButton hidden className="settingsButton">
                         <Settings style={{ fontSize: 35 }} />
                     </IconButton>
                 </div>
@@ -112,8 +185,8 @@ function Group() {
             <div className="dateBlock">
                 <div>
                     <div>
-                        <p>5</p>
-                        <p>сентябрь</p>
+                        <p>{dateFormat.downDay}</p>
+                        <p>{dateFormat.getMonth(dateFormat.dDownDay)}</p>
                         <div>
                             <div className="circle"></div>
                             <div className="stripe"></div>
@@ -122,8 +195,8 @@ function Group() {
                 </div>
                 <div>
                     <div className="rightDateContainer">
-                        <p>10</p>
-                        <p>декабрь</p>
+                        <p>{dateFormat.upDay}</p>
+                        <p>{dateFormat.getMonth(dateFormat.dUpDay)}</p>
                         <div className="rightStripeContainer">
                             <div className="stripe-right"></div>
                             <div className="circle-right"></div>
@@ -133,19 +206,16 @@ function Group() {
                 <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={locale}>
                     <MobileDatePicker className="weekDate"
                         value={value}
-                        onChange={(newValue) => {
-                            setValue(newValue);
-                        }}
+                        onChange={handleDateChange}
                         renderInput={(params) => <TextField {...params} />}
                     >
-
                     </MobileDatePicker>
                 </LocalizationProvider>
-
             </div>
             <div className="shedule">
                 <p>LastDance</p>
                 {spisok}
+                <img className="reloadCat" src={ReloadCat}></img>
             </div>
         </div>
     );
@@ -180,17 +250,29 @@ class GroupSelect extends React.Component {
     }
 
     handleChange = (e) => {
-        this.setState({ group: e.target.value })
-        axios.get(`http://192.168.147.51:81/api/lastdance/getgroup/${e.target.value}`)
+        let img = document.querySelector('.reloadCat');
+        let itemsShedule = document.querySelectorAll('.shedule *:not(img)')
+        img.style = "opacity: 1; z-index: 3";
+        let doc = document.querySelector('.css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input').value.split('.');
+        this.setState({ group: e.target.value, })
+        this.props.handleGroupChange([]);
+        axios.get(`http://192.168.147.51:81/api/lastdance/getgroupmobile/${e.target.value}?Date=${doc[1]}.${doc[0]}.${doc[2]}`)
             .then((response) => {
                 if (response.status === 200) {
                     console.log(response.data);
                     this.props.handleGroupChange(response.data);
+
                 }
             }).catch(err => {
                 console.log(err);
             })
-
+        setTimeout(() => {
+            img.style = "opacity: 0; z-index: 1";
+            itemsShedule = document.querySelectorAll('.shedule > *:not(img)');
+            itemsShedule.forEach(element => {
+                element.style = "";
+            });
+        }, 3000);
     }
 
     selectItem() {
@@ -202,7 +284,7 @@ class GroupSelect extends React.Component {
     }
     render() {
         return (
-            <Select variant="standard" value={this.group} onChange={this.handleChange} IconComponent={undefined} className="groupSelect" >
+            <Select variant="standard" value={this.state.group} onChange={this.handleChange} IconComponent={undefined} className="groupSelect" >
                 {this.selectItem()}
             </Select>
         );
